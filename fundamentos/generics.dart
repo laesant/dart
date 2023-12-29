de4587cd5b8e40a8d363e2936e816c6c04bfd048
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-12-29 11:31:28 -0300

    Usando Generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> frutas = ['banana', 'maçã', 'laranja'];
+  frutas.add('melancia');
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiário': 600.00
+  };
+
+  print(salarios);
+}
