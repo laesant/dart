92badd4260c999cfc6ea8d0f5d0f0e854b82ae61
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-01-02 10:46:22 -0300

    Desafio If/Else

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,10 @@
+void main() {
+  var nota = 2.3;
+
+  if (nota >= 9.0) // ; NÃO USA ; EM ESTRUTURAS DE CONTROLE DESSA FORMA! *
+  {
+    print('Parabéns! Você foi brilhante!');
+  }
+
+  // * EXCEÇÃO DO/WHILE
+}
