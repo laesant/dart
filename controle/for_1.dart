ae937cf7e83c35b607e2a6f575cb7a1329eedb6c
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-01-02 11:30:39 -0300

    For #2

--- controle/for_1.dart
+++ controle/for_1.dart
@@ -8,5 +8,11 @@ void main() {
     print('b = $b');
   }
   print('[FORA] b = $b');
+
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+  for (var i = 0; i < notas.length; i++) {
+    print("Notas ${i + i} = ${notas[i]}.");
+  }
+  
   print("Fim!");
 }
