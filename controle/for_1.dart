bd36146c0317eec311f925b146cd6baf1f43b57d
Laênio Marques <105957017+laesant@users.noreply.github.com>
2024-01-02 10:56:25 -0300

    For #01

---
+++ controle/for_1.dart
@@ -0,0 +1,12 @@
+void main() {
+  for (int a = 100; a >= 0; a -= 4) {
+    print("a = $a");
+  }
+
+  int b = 0;
+  for (b; b <= 10; b++) {
+    print('b = $b');
+  }
+  print('[FORA] b = $b');
+  print("Fim!");
+}
